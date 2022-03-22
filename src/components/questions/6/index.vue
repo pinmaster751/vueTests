723d8d4abc67d7091b28706a7feb961b055363ec
pinmaster751@gmail.com <dropdown>
2022-03-22 13:47:17 +0300

    task6

--- src/components/questions/6/index.vue
+++ src/components/questions/6/index.vue
@@ -1,6 +1,5 @@
 <template>
   <div>
-    <!-- TODO: при нажатии на кнопку "Enter Email" инпут должен появляться с фокусом на нем-->
     <input v-if="showEmailInput" name="email" ref="emailInput">
     <button @click="showEmailInput = true">Enter Email</button>
   </div>
