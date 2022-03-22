f9dd927bddf4561a7e4eef5c080dc8ce3145d80e
pinmaster751@gmail.com <dropdown>
2022-03-22 12:14:52 +0300

    task2

--- src/components/questions/2/index.vue
+++ src/components/questions/2/index.vue
@@ -1,5 +1,4 @@
 <template>
-  <!-- FIXME: отправка формы перезагружает страницу после submit -->
   <form @submit.prevent="onSubmit">
     <input placeholder="name" v-model="name" id="name" /><br  /><br />
     <input placeholder="last name" v-model="lastName" id="lastName" />
